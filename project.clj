3c8513f3032b8d866ce356f80886adff20074bf8
Steven Zhou <zzuohong@gmail.com>
2019-02-07 17:05:06 -0500

    Bootstrap p7.

--- project.clj
+++ project.clj
@@ -12,7 +12,8 @@
                  [org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.238"]
                  [org.clojure/core.async  "0.4.474"]
-                 [reagent "0.7.0"]]
+                 [reagent "0.7.0"]
+                 [re-com "2.4.0"]]
 
   :plugins [[lein-figwheel "0.5.16"]
             [lein-cljsbuild "1.1.7" :exclusions [[org.clojure/clojure]]]]
--- src/console/core.cljs
+++ src/console/core.cljs
@@ -1,58 +1,142 @@
 (ns console.core
-  (:require [reagent.core :as reagent :refer [atom]]
-            [cljs-http.client :as http]))
+  (:require [cljs-http.client :as http]
+            [reagent.core :as reagent :refer [atom]]
+            [re-com.core :as rc :refer [box
+                                        button
+                                        h-box
+                                        info-button
+                                        input-text
+                                        label
+                                        p
+                                        single-dropdown
+                                        throbber
+                                        title
+                                        v-box]]))
 
 (enable-console-print!)
 
-(println "This text is printed from src/console/core.cljs. Go ahead and edit it and see reloading in action.")
-
-;; define your app data so that it doesn't get over-written on reload
-
-(defonce app-state (atom {:text "Hello world!"}))
+(def app-state (atom {:invoking? false}))
 
 (def action (atom nil))
-(def ns (atom nil))
+(def nspace (atom nil))
 (def target (atom nil))
+(def charts-branch (atom nil))
+(def target-branch (atom nil))
 
-(defn hello-world []
-  [:div
-   [:input {:type :button
-            :value "Invoker home page"
-            :on-click #(http/get "http://invoker.admin:8080")}]
-   [:h3 "Action"]
-   [:div
-    [:input {:type :button
-             :value "install"
-             :on-click #(reset! action :install)}]
-    [:input {:type :button
-             :value "upgrade"
-             :on-click #(reset! action :upgrade)}]
-    [:input {:type :button
-             :value "delete"
-             :on-click #(reset! action :delete)}]]
-   [:h3 "Target"]
-   [:div
-    [:input {:type :text
-             :value @target
-             :on-change #(reset! target (-> % .-target .-value))}]]
-   [:h3 "Namespace"]
-   [:div
-    [:input {:type :text
-             :value @ns
-             :on-change #(reset! ns (-> % .-target .-value))}]]
-   [:h3 "Your command is"]
-   [:div
-    [:strong @action] " target " [:strong @target] " in namespace " [:strong @ns]]
-   [:div
-    [:input {:type :button
-             :value "invoke"
-             :on-click #(http/post "http://invoker.admin:8080/api/v0.1/invoke"
-                                   {:json-params {:action @action
-                                                  :namespace @ns
-                                                  :target @target}})}]]])
+(def action-choices
+  [{:id :install :label "install"}
+   {:id :upgrade :label "upgrade"}
+   {:id :delete :label "delete"}])
 
-(reagent/render-component [hello-world]
-                          (. js/document (getElementById "app")))
+(defn action-dropdown
+  []
+  [v-box
+   :children [[h-box
+               :gap "3px"
+               :children [[:span.field-label "action"]
+                          [info-button
+                           :info "Cluster action you want to make."
+                           :width "200px"]]]
+              [single-dropdown
+               :choices action-choices
+               :model action
+               :on-change #(reset! action %)
+               :width "120px"]]])
+
+(defn target-input
+  []
+  [v-box
+   :children [[h-box
+               :gap "3px"
+               :children [[:span.field-label "target"]
+                          [info-button
+                           :info "Project's name on which your action is applied."
+                           :width "200px"]]]
+              [input-text
+               :model target
+               :on-change #(reset! target %)
+               :width "120px"]]])
+
+(defn namespace-input
+  []
+  [v-box
+   :children [[h-box
+               :gap "3px"
+               :children [[:span.field-label "namespace"]
+                          [info-button
+                           :info "Cluster namespace in which you action is scoped."
+                           :width "200px"]]]
+              [input-text
+               :model nspace
+               :on-change #(reset! nspace %)
+               :width "120px"]]])
+
+(defn charts-branch-input
+  []
+  [v-box
+   :children [[h-box
+               :gap "3px"
+               :children [[:span.field-label "charts branch"]
+                          [info-button
+                           :info (str "(Optional) The Git branch of your charts"
+                                      " repo, default to master.")
+                           :width "200px"]]]
+              [input-text
+               :model charts-branch
+               :on-change #(reset! charts-branch %)
+               :placeholder "master"
+               :width "120px"]]])
+
+(defn target-branch-input
+  []
+  [v-box
+   :children [[h-box
+               :gap "3px"
+               :children [[:span.field-label "target branch"]
+                          [info-button
+                           :info (str "(Optional) The Git branch of your target"
+                                      " repo, default to master.")
+                           :width "200px"]]]
+              [input-text
+               :model target-branch
+               :on-change #(reset! target-branch %)
+               :placeholder "master"
+               :width "120px"]]])
+
+(defn console-panel
+  []
+  [v-box
+   :size "auto"
+   :gap "30px"
+   :children [[title
+               :label "Invoker console"
+               :level :level1]
+              [title
+               :label "Combine reagents and invoke the spell as you want."
+               :level :level4]
+              [h-box
+               :size "auto"
+               :gap "10px"
+               :children [[action-dropdown]
+                          [target-input]
+                          [namespace-input]
+                          [charts-branch-input]
+                          [target-branch-input]]]
+              [h-box
+               :children [[button
+                           :class "btn-primary"
+                           :label "invoke"
+                           :on-click
+                           #(do
+                              (swap! app-state assoc :invoking? true)
+                              (prn @action @charts-branch @nspace @target @target-branch))
+                           :tooltip "Run the command in your cluster."
+                           :tooltip-position :below-center]
+                          (when (:invoking? @app-state) [throbber])]
+               :gap "10px"
+               :size "auto"]]])
+
+(reagent/render-component [console-panel] (. js/document (getElementById "app")))
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
